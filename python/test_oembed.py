d45d78ac38952a30c6031e108386c0a4b2e6fcb9
KADARI ESHWAR <174132605+Eshwarkadari@users.noreply.github.com>
2026-06-18 11:42:40 +0530

    debug: test PinResource API for is_video detection

--- python/test_oembed.py
+++ python/test_oembed.py
@@ -1,45 +1,50 @@
 import requests, json, urllib.parse, os, base64, time
 
-pin_urls = [
-    "https://www.pinterest.com/pin/872361390330023115/",
-    "https://www.pinterest.com/pin/1127940669175905650/",
-]
+pin_ids = ["872361390330023115", "1127940669175905650"]
 
 output_lines = []
 def log(msg):
     print(msg)
     output_lines.append(str(msg))
 
-for pin_url in pin_urls:
+for pin_id in pin_ids:
     log("")
-    log("=== Testing: " + pin_url + " ===")
-    for ua in [
-        "Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)",
-        "facebookexternalhit/1.1",
-    ]:
-        try:
-            r = requests.get(
-                "https://www.pinterest.com/oembed.json?url=" + urllib.parse.quote(pin_url),
-                timeout=15,
-                headers={"User-Agent": ua, "Accept": "application/json"})
-            log("  UA=" + ua[:30] + " HTTP=" + str(r.status_code))
-            if r.status_code == 200:
-                d = r.json()
-                log("  type: " + str(d.get('type')))
-                log("  thumbnail_url: " + str(d.get('thumbnail_url','')))
-                log("  html: " + str(d.get('html','')))
-                log("  ALL FIELDS JSON: " + json.dumps(d))
-            else:
-                log("  body: " + r.text[:200])
-        except Exception as e:
-            log("  ERROR: " + str(e))
+    log("=== PinResource API for pin_id: " + pin_id + " ===")
+    try:
+        api_url = (
+            "https://www.pinterest.com/resource/PinResource/get/"
+            "?source_url=/pin/" + pin_id + "/"
+            "&data=%7B%22options%22%3A%7B%22id%22%3A%22" + pin_id + "%22%2C%22field_set_key%22%3A%22detailed%22%7D%7D"
+        )
+        r = requests.get(api_url, timeout=15, headers={
+            "User-Agent": "Mozilla/5.0 (compatible; Googlebot/2.1)",
+            "Accept": "application/json",
+            "X-Requested-With": "XMLHttpRequest",
+            "Referer": "https://www.pinterest.com/",
+        })
+        log("HTTP: " + str(r.status_code) + " | len=" + str(len(r.text)))
+        if r.status_code == 200:
+            data = r.json()
+            # Navigate to the pin data
+            resource_response = data.get("resource_response", {})
+            pin_data = resource_response.get("data", {})
+            log("Pin data keys: " + str(list(pin_data.keys())[:30]))
+            log("is_video: " + str(pin_data.get("is_video")))
+            log("videos present: " + str("videos" in pin_data))
+            if "videos" in pin_data:
+                log("videos value: " + json.dumps(pin_data["videos"])[:300])
+            log("media: " + json.dumps(pin_data.get("media", {}))[:200])
+            log("type field: " + str(pin_data.get("type")))
+        else:
+            log("Body: " + r.text[:200])
+    except Exception as e:
+        log("ERROR: " + str(e))
 
 result_text = "\n".join(output_lines)
-print("RESULT_TEXT_LENGTH:", len(result_text))
+print("RESULT LENGTH:", len(result_text))
 
 GH_TOKEN = os.environ.get("DEBUG_GH_TOKEN", "")
 REPO = os.environ.get("GITHUB_REPO", "Eshwarkadari/Instagram-Auto-Poster")
-print("Token present:", bool(GH_TOKEN))
 
 b64content = base64.b64encode(result_text.encode("utf-8")).decode("utf-8")
 sha = None
@@ -47,11 +52,10 @@ r = requests.get(
     "https://api.github.com/repos/" + REPO + "/contents/oembed_debug_output.txt",
     headers={"Authorization": "Bearer " + GH_TOKEN, "Accept": "application/vnd.github+json"},
     timeout=15)
-print("SHA check status:", r.status_code)
 if r.status_code == 200:
     sha = r.json().get("sha")
 
-payload = {"message": "debug: oEmbed output " + str(int(time.time())), "content": b64content}
+payload = {"message": "debug: PinResource output " + str(int(time.time())), "content": b64content}
 if sha:
     payload["sha"] = sha
 
@@ -60,4 +64,3 @@ r2 = requests.put(
     headers={"Authorization": "Bearer " + GH_TOKEN, "Accept": "application/vnd.github+json"},
     json=payload, timeout=30)
 print("PUSH_STATUS:", r2.status_code)
-print("PUSH_BODY:", r2.text[:300])
