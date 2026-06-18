1176f73dc855f05e2a9e330df1cc67e9eb25d14c
KADARI ESHWAR <174132605+Eshwarkadari@users.noreply.github.com>
2026-06-18 11:44:48 +0530

    debug: test embed.html for video signals

--- python/test_oembed.py
+++ python/test_oembed.py
@@ -1,4 +1,4 @@
-import requests, json, urllib.parse, os, base64, time
+import requests, json, urllib.parse, os, base64, time, re
 
 pin_ids = ["872361390330023115", "1127940669175905650"]
 
@@ -9,39 +9,34 @@ def log(msg):
 
 for pin_id in pin_ids:
     log("")
-    log("=== PinResource API for pin_id: " + pin_id + " ===")
+    log("=== embed.html for pin_id: " + pin_id + " ===")
+    embed_url = "https://assets.pinterest.com/ext/embed.html?id=" + pin_id + "&src=oembed"
     try:
-        api_url = (
-            "https://www.pinterest.com/resource/PinResource/get/"
-            "?source_url=/pin/" + pin_id + "/"
-            "&data=%7B%22options%22%3A%7B%22id%22%3A%22" + pin_id + "%22%2C%22field_set_key%22%3A%22detailed%22%7D%7D"
-        )
-        r = requests.get(api_url, timeout=15, headers={
+        r = requests.get(embed_url, timeout=15, headers={
             "User-Agent": "Mozilla/5.0 (compatible; Googlebot/2.1)",
-            "Accept": "application/json",
-            "X-Requested-With": "XMLHttpRequest",
+            "Accept": "text/html",
             "Referer": "https://www.pinterest.com/",
         })
         log("HTTP: " + str(r.status_code) + " | len=" + str(len(r.text)))
         if r.status_code == 200:
-            data = r.json()
-            # Navigate to the pin data
-            resource_response = data.get("resource_response", {})
-            pin_data = resource_response.get("data", {})
-            log("Pin data keys: " + str(list(pin_data.keys())[:30]))
-            log("is_video: " + str(pin_data.get("is_video")))
-            log("videos present: " + str("videos" in pin_data))
-            if "videos" in pin_data:
-                log("videos value: " + json.dumps(pin_data["videos"])[:300])
-            log("media: " + json.dumps(pin_data.get("media", {}))[:200])
-            log("type field: " + str(pin_data.get("type")))
+            html = r.text
+            log("has <video: " + str("<video" in html.lower()))
+            log("has video_url: " + str("video_url" in html))
+            log("has .mp4: " + str(".mp4" in html))
+            log("has v.pinimg: " + str("v.pinimg" in html))
+            log("has og:video: " + str("og:video" in html))
+            log("has play button class: " + str("playButton" in html or "PlayButton" in html or "play-button" in html))
+            log("has isVideo: " + str("isVideo" in html or "is_video" in html))
+            # Find script tags
+            scripts = re.findall(r'<script[^>]*>([\s\S]{0,200})', html)
+            log("script count: " + str(len(scripts)))
+            log("HTML preview (first 800): " + html[:800].replace("\n"," "))
         else:
             log("Body: " + r.text[:200])
     except Exception as e:
         log("ERROR: " + str(e))
 
 result_text = "\n".join(output_lines)
-print("RESULT LENGTH:", len(result_text))
 
 GH_TOKEN = os.environ.get("DEBUG_GH_TOKEN", "")
 REPO = os.environ.get("GITHUB_REPO", "Eshwarkadari/Instagram-Auto-Poster")
@@ -54,11 +49,9 @@ r = requests.get(
     timeout=15)
 if r.status_code == 200:
     sha = r.json().get("sha")
-
-payload = {"message": "debug: PinResource output " + str(int(time.time())), "content": b64content}
+payload = {"message": "debug: embed.html output " + str(int(time.time())), "content": b64content}
 if sha:
     payload["sha"] = sha
-
 r2 = requests.put(
     "https://api.github.com/repos/" + REPO + "/contents/oembed_debug_output.txt",
     headers={"Authorization": "Bearer " + GH_TOKEN, "Accept": "application/vnd.github+json"},
